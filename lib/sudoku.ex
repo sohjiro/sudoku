790810d541de0e55ada6ee41cdffff73b8e61261
Sohjiro <felipe@makingdevs.com>
2020-03-04 22:38:50 -0600

    enabling quadrants

--- lib/sudoku.ex
+++ lib/sudoku.ex
@@ -13,7 +13,7 @@ defmodule Sudoku do
   def eval(text) do
     text
     |> fetch_size_and_values()
-    |> valid_input?()
+    |> sudoku_valid?()
   end
 
   def fetch_size_and_values(text) do
@@ -21,13 +21,71 @@ defmodule Sudoku do
     {parse(size), values}
   end
 
-  defp valid_input?({size, values}) do
-    rows = board(size, values)
-    cols = transpose(rows)
+  defp sudoku_valid?({size, values}) do
+    case validate_sudoku(size, values) do
+      {true, true, true, _board} ->
+        true
+      _ ->
+        false
+    end
+  end
 
-    rows
-    |> concat_rows_and_columns(cols)
-    |> valid?(size)
+  defp validate_sudoku(size, values) do
+    board = board(size, values)
+
+    row = valid_rows?(board)
+
+    col =
+      board
+      |> transpose()
+      |> valid_rows?()
+
+    blocks =
+      size
+      |> quadrants()
+      |> to_blocks(board)
+      |> valid_rows?()
+
+    {row, col, blocks, board}
+  end
+
+  defp generate_coordinates(blocks) do
+    Enum.map(blocks, fn({q_x, q_y}) ->
+      for x <- q_x, y <- q_y, do: {x, y}
+    end)
+  end
+
+  defp to_blocks(blocks, board) do
+    blocks
+    |> generate_coordinates()
+    |> Enum.map(&(extract_values_from_coordinates(&1, board)))
+  end
+
+  defp extract_values_from_coordinates(row_coordinates, board) do
+    Enum.map(row_coordinates, fn({x, y}) ->
+      board
+      |> Enum.at(x)
+      |> Enum.at(y)
+    end)
+  end
+
+  defp quadrants(size) do
+    quadrant_size = floor(:math.sqrt(size))
+    quadrant_total = 0..(size-1)
+
+    for block <- quadrant_total do
+      q_x = div(block, quadrant_size) * quadrant_size
+      q_y = rem(block, quadrant_size) * quadrant_size
+      {q_x..(q_x + quadrant_size - 1), q_y..(q_y + quadrant_size - 1)}
+    end
+  end
+
+  defp valid_rows?(board) do
+    1 ===
+    board
+    |> Enum.flat_map(&frequencies/1)
+    |> Enum.uniq()
+    |> length()
   end
 
   defp board(size, values) do
@@ -42,39 +100,16 @@ defmodule Sudoku do
     |> Enum.map(&Tuple.to_list/1)
   end
 
-  defp concat_rows_and_columns(rows, cols) do
-    List.zip(rows ++ cols)
-  end
-
   defp parse(size) do
     String.to_integer(size)
   end
 
-  defp valid?(rows, _size) do
-    rows
-    |> fetch_frequencies()
-    |> valid_frequencies()
-  end
-
-  defp fetch_frequencies(rows) do
-    Enum.reduce(rows, [], &([frequencies(&1) | &2]))
-  end
-
   defp frequencies(row) do
     row
-    |> Tuple.to_list()
     |> Enum.reduce(%{}, fn(data, acc) ->
       Map.update(acc, data, 1, &(&1 + 1))
     end)
-  end
-
-  defp valid_frequencies(frequencies) do
-    frequencies
-    |> Enum.map(fn(row) ->
-      row
-      |> Map.values()
-      |> Enum.all?(&(&1 === 2))
-    end)
-    |> Enum.all?()
+    |> Map.values()
+    |> Enum.uniq()
   end
 end
